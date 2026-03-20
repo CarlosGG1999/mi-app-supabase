dd3d987d9fe7fa7de9f1fdd44c5b859a13e36277
CarlosGG1999 <c.hugogarcia06@gmail.com>
2026-03-19 20:01:49 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ierbqujiilmrejbgiflm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcmJxdWppaWxtcmVqYmdpZmxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NTU0OTUsImV4cCI6MjA4OTUzMTQ5NX0.lAZ9kt0MV_1NcDEAtMkOL1M3_J93phlR7AsH6ueDyKo'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function obtenerEstudiantes() {
+  const { data, error } = await supabase
+    .from('estudiantes')
+    .select('*')
+
+  if (error) {
+    console.log('Error:', error)
+  } else {
+    console.log('Datos:', data)
+  }
+}
+
+obtenerEstudiantes();
